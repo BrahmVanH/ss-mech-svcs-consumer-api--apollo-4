609aa5b55addca4492403c95c35ce32ddd1bfb8a
Brahm Van Houzen <brahmvanh@gmail.com>
2024-07-05 17:13:06 -0400

    added origin declaration to header for apollo client

--- src/lib/apolloClient/index.ts
+++ src/lib/apolloClient/index.ts
@@ -4,6 +4,9 @@ const client = new ApolloClient({
 	cache: new InMemoryCache(),
 	link: new HttpLink({
 		uri: process.env.CRM_LAMBDA_FUNC_URL ?? 'http://localhost:5000/graphql',
+		headers: {
+			origin: process.env.CORS_ORIGIN ?? 'http://localhost:4000',
+		},
 	}),
 	defaultOptions: {
 		query: {
